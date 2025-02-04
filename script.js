68a7c6554a6c9cb13d9bca1df22a24c6886c51f3
Md. Forhad Hossain <141973117+m-forhad@users.noreply.github.com>
2025-02-04 10:14:46 +0600

    update script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const text1 = document.getElementById("text1");
+    const text1 = document.getElementById("text1");
     const text2 = document.getElementById("text2");
     const text3 = document.getElementById("text3");
     const text4 = document.getElementById("text4");
